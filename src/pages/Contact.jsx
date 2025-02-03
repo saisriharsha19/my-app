a6927ef0caf7e83bff03bb37f707b5d5a2639879
saisriharsha19 <149857181+saisriharsha19@users.noreply.github.com>
2025-02-03 18:46:19 -0500

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('http://127.0.0.1:8000/contact/', {
+      const response = await fetch('https://personalwebsitebackend-gthafrgadzc2argc.eastus2-01.azurewebsites.net/contact/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
