45dbe982ffa890cb9422f881a4c20276282150d4
saisriharsha19 <sriharsha121925@gmail.com>
2025-02-06 22:53:40 -0500

    updated code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import favicon from "./icons/favicon.ico"; // Import the favicon
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Dynamically add favicon to head
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = favicon;
+document.head.appendChild(link);
+document.title = "Sai Sri Harsha Guddati Portfolio";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
