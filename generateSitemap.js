6756aeb2f5102e272e3df813094b42cdff95334c
saisriharsha19 <sriharsha121925@gmail.com>
2026-02-25 11:05:32 -0500

    chore: update sitemap base URL constant.

--- generateSitemap.js
+++ generateSitemap.js
@@ -16,7 +16,7 @@ async function fetchBlogPosts() {
 }
 
 async function generateSitemap() {
-  const BASE_URL = "https://saisriharsha.me";
+  const BASE_URL = "https://www.saisriharsha.com";
   const smStream = new SitemapStream({ hostname: BASE_URL });
 
   const staticPages = [
